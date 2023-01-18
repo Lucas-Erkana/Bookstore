1ffd80fcd26073bc40bd9a791e16aaf40c7070f3
Lucas David Erkana <lderkana21x@gmail.com>
2023-01-18 02:20:19 +0200

    Add catgeories.js to page folder

---
+++ src/pages/Categories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Categories = () => (
+  <div className="categories">
+    <div className="categ_text">
+      <button type="button">Check status</button>
+    </div>
+  </div>
+);
+
+export default Categories;
