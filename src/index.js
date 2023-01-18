86907d3d758fbfdd6519313885727e88bfe6c4c9
Lucas David Erkana <lderkana21x@gmail.com>
2023-01-18 02:13:24 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router> 
+  <React.StrictMode>
     <App />
-  </Router>
+  </React.StrictMode>,
 );
