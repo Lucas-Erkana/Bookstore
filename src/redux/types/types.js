5ee84a0a9ba7073f02e085a9e1cfe0c1de9b4b8d
Lucas David Erkana <lderkana21x@gmail.com>
2023-01-20 11:25:58 +0200

    Add types.js

---
+++ src/redux/types/types.js
@@ -0,0 +1,5 @@
+const ADDED_BOOK = 'redux/books/BOOK_ADDED';
+const REMOVED_BOOK = 'redux/books/BOOK_REMOVED';
+const BOOK_STATUS_CHECK = 'redux/categories/CHECKED_BOOK_STATUS';
+
+export default { ADDED_BOOK, REMOVED_BOOK, BOOK_STATUS_CHECK };
