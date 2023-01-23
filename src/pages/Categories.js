59eb979b558c5053b9c54241c5494728cb6a8235
Lucas David Erkana <lderkana21x@gmail.com>
2023-01-23 12:23:11 +0200

    import useDispatch and useDispatch

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkStatus } from '../redux/categories/categories';
 
-const Categories = () => (
-  <div className="categories">
-    <div className="categ_text">
-      <button type="button">Check status</button>
+const Categories = () => {
+  const selectedCategories = useSelector((state) => state.categories);
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    dispatch(checkStatus());
+  };
+
+  return (
+    <div className="categories">
+      <div className="categ_text">
+        <h2>{selectedCategories.categories[0]}</h2>
+        <button type="button" onClick={handleClick}>
+          Check status
+        </button>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Categories;
