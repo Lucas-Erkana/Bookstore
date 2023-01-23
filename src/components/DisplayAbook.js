ee0099037774e48d2a655fa7e547bec7add15108
Lucas David Erkana <lderkana21x@gmail.com>
2023-01-23 12:15:50 +0200

    Updte DisplayAbook.js

--- src/components/DisplayAbook.js
+++ src/components/DisplayAbook.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removingBook } from '../redux/books/books';
 
 const DisplayAbook = (props) => {
-  const { Title, Author } = props;
+  const { Title, Author, Id } = props;
+
+  const dispatch = useDispatch();
+
+  const handleRemove = () => {
+    dispatch(removingBook(Id));
+  };
+
   return (
     <li className="book_item">
       <h2>{Title}</h2>
       <h4>{Author}</h4>
-      <button type="button">Remove</button>
+      <button type="button" onClick={handleRemove}>
+        Remove
+      </button>
     </li>
   );
 };
@@ -15,6 +26,7 @@ const DisplayAbook = (props) => {
 DisplayAbook.propTypes = {
   Title: PropTypes.string.isRequired,
   Author: PropTypes.string.isRequired,
+  Id: PropTypes.string.isRequired,
 };
 
 export default DisplayAbook;
